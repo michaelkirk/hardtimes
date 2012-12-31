59ccb015c7c3fd0f8fad1e2c3ecd032766a57f78
Michael Kirk <michael.john.kirk@gmail.com>
2012-12-31 16:16:31 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hardtimes::Application.config.secret_token = 'b964a0888a95517658e7b4e31679409dbfebc1294b8dd4d27000ed63d4b40ac7aacf66d7091c80ca6656dc7ec07c68ac965810f7b780af9d885578b76b5f120d'
