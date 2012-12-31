66cd9c64b0a70c356a9d3e566080b23d730bb3f6
Michael Kirk <michael.john.kirk@gmail.com>
2012-12-31 17:59:46 -0600

    subdomain wankery

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,11 +4,10 @@ class VisitorsController < ApplicationController
   end
 
   def index
-
   end
 
   def show
-    @visitor_name = params[:visitor_name]
+    @visitor_name = params[:visitor_name] || request.subdomain
     @visit = Visit.for_visitor_name(@visitor_name).last
 
     if(@visit.nil? or !@visit.still_at_hard_times?)
@@ -19,13 +18,13 @@ class VisitorsController < ApplicationController
   end
 
   def arrive
-    @visitor_name = params[:visitor_name]
+    @visitor_name = params[:visitor_name] || request.subdomain
     @visit = Visit.for_visitor_name(@visitor_name).create!(arrived_at: Time.now)
     redirect_to visitor_url(@visitor_name)
   end
 
   def leave
-    @visitor_name = params[:visitor_name]
+    @visitor_name = params[:visitor_name] || request.subdomain
     @visit = Visit.for_visitor_name(@visitor_name).last
     raise Error.new("A person not at hardtimes can't leave hardtimes.") if @visit.nil?
 
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,9 @@ Hardtimes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'visitors#index'
+  get '/', :to => 'visitors#index', :constraints => {:subdomain => ""}
+  get '/', :to => 'visitors#show'
+
   get '/visitors/search', :to => 'visitors#search', :as => 'visitors_search'
   get '/visitors/:visitor_name', :to => 'visitors#show', :as => 'visitor'
   post '/visitors/:visitor_name/arrive', :to => 'visitors#arrive', :as => 'visitor_arrival'
